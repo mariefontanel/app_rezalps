5e05e8b59932da50e78cf61ecd406ac56c6564d3
Votre Michael <jouvemichael@gmail.com>
2018-06-01 11:18:00 +0200

    folder img

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-/**
- *
- */
+
 import React, { Component } from 'react';
 import Header from './src/header';
 import {
@@ -10,9 +8,19 @@ import {
     Image,
     ScrollView,
     View,
+    Button
 
 
 } from 'react-native';
+
+import {
+    StackNavigator,
+} from 'react-navigation';
+
+// const App = StackNavigator({
+//     accueil: { screen: AccueilScreen },
+//     test: { screen: testscreen },
+// });
 
 
 
@@ -37,44 +45,67 @@ export default class App extends Component {
                     <Image source={require('./public/img/recrutement.png')} style={styles.imgIntro} />
                     <Image source={require('./public/img/link.png')} style={styles.imgIntro} />
                 </View>
-                <View style={{flex: 2, flexDirection: 'row', justifyContent:'space-around', marginTop: 5, marginBottom: 5,}}>
-                    <text>partage</text>
-                    <text>recrutement</text>
-                    <text>lien</text>
+                <View style={{flex: 2, flexDirection: 'row', justifyContent:'space-around', marginTop: 4, marginBottom: 7,}}>
+                    <Text>Apprendre</Text>
+                    <Text>Recruter</Text>
+                    <Text>Rencontrer</Text>
                 </View>
 
                 <ScrollView>
                     <Text style={styles.postsIndex}>
-                        Welcome to Rezalps!
+                        Ici seront les 5 derniers posts du réseau rezalps !
                     </Text>
                 </ScrollView>
+                {/*<view style={{flex: 3, flexDirection: 'row', justifyContent:'space-around', marginTop: 5, marginBottom: 5,}}>*/}
+                {/*<Button*/}
+                    {/*style = {styles.button}*/}
+                    {/*onPress={}*/}
+                    {/*title="Inscription"*/}
+                    {/*color="#841584"*/}
+                    {/*accessibilityLabel="Learn more about this purple button"*/}
+                {/*/>*/}
+                {/*<Button*/}
+                    {/*style = {styles.button}*/}
+                    {/*onPress={}*/}
+                    {/*title="Login"*/}
+                    {/*color="#841584"*/}
+                    {/*accessibilityLabel="Learn more about this purple button"*/}
+                {/*/>*/}
+                {/*</view>*/}
+
+
+
             </View>
-    )
+        )
     }
-    }
+}
 
 
-    const styles = StyleSheet.create({
+const styles = StyleSheet.create({
 
-        container: {
+    container: {
         backgroundColor: '#F5FCFF',
     },
-        intro: {
-
+    intro: {
         flexDirection: 'row',
     },
-        imgIntro: {
+    imgIntro: {
         height: 50,
         width: 50,
     },
-
-        postsIndex: {
+    postsIndex: {
         backgroundColor: 'blue',
         marginBottom: 5,
         height: 150,
     },
-        avatars:{
+    avatars: {
         width: 193,
         height: 210,
     },
-    });
+    button: {
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 10,
+        marginRight: 10,
+    }
+});
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Accueil from './App';
 
-AppRegistry.registerComponent('app_rezalps', () => App);
+AppRegistry.registerComponent('app_rezalps', () => Accueil);
